f4b3a8fbcd3f8c69464f01ef9d48a701fb5a33c4
Faizal <34762675+faizalzafri@users.noreply.github.com>
2020-07-21 14:27:47 +0530

    enabling consumer

---
+++ src/main/java/com/github/faizal/libraryeventskafka/config/ConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.github.faizal.libraryeventskafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class ConsumerConfig {
+}
