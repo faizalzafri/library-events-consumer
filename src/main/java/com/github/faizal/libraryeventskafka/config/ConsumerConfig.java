1c72b575be217062c7bf0c4b2183a0faee689c12
Faizal <34762675+faizalzafri@users.noreply.github.com>
2020-07-23 00:49:18 +0530

    Custom error handler
    adding error handle in a default container factory.

--- src/main/java/com/github/faizal/libraryeventskafka/config/ConsumerConfig.java
+++ src/main/java/com/github/faizal/libraryeventskafka/config/ConsumerConfig.java
@@ -1,9 +1,22 @@
 package com.github.faizal.libraryeventskafka.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class ConsumerConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactory factory){
+        factory.setErrorHandler((thrownException, data) -> {
+            log.info("Error {}. Data: {}",thrownException.getMessage(), data.value());
+        });
+        return factory;
+    }
 }
