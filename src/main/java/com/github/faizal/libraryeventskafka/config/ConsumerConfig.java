300bfd70d09a7b780bc4630454f19d019f98347f
Faizal <34762675+faizalzafri@users.noreply.github.com>
2020-07-24 13:34:01 +0530

    retry, retry template, backoff,

--- src/main/java/com/github/faizal/libraryeventskafka/config/ConsumerConfig.java
+++ src/main/java/com/github/faizal/libraryeventskafka/config/ConsumerConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @EnableKafka
@@ -17,6 +21,26 @@ public class ConsumerConfig {
         factory.setErrorHandler((thrownException, data) -> {
             log.info("Error {}. Data: {}",thrownException.getMessage(), data.value());
         });
+        factory.setRetryTemplate(retryTemplate());
         return factory;
+    }
+
+    // returs a retry template
+    private RetryTemplate retryTemplate() {
+
+        // back of 1000ms before retry
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(1000);
+
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(simpleRetryPolicy());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        return retryTemplate;
+    }
+
+    private RetryPolicy simpleRetryPolicy() {
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+        return simpleRetryPolicy;
     }
 }
